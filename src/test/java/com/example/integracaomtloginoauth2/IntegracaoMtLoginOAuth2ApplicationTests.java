6b77ebbc890b5ff35c914ab4c7e7d432f27bbdd4
01923483102 <leomatisa1000@gmail.com>
2025-07-16 11:29:41 -0400

    fix: ignorar configurações que dependem de rede para teste

--- src/test/java/com/example/integracaomtloginoauth2/IntegracaoMtLoginOAuth2ApplicationTests.java
+++ src/test/java/com/example/integracaomtloginoauth2/IntegracaoMtLoginOAuth2ApplicationTests.java
@@ -1,10 +1,15 @@
 package com.example.integracaomtloginoauth2;
 
+import com.example.integracaomtloginoauth2.config.KeycloakAdminClientConfig;
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.autoconfigure.security.oauth2.client.servlet.OAuth2ClientAutoConfiguration;
+import org.springframework.boot.autoconfigure.security.oauth2.resource.servlet.OAuth2ResourceServerAutoConfiguration;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
 
-@SpringBootTest
+@SpringBootTest(properties = {
+        "spring.autoconfigure.exclude=org.springframework.boot.autoconfigure.security.oauth2.client.servlet.OAuth2ClientAutoConfiguration,org.springframework.boot.autoconfigure.security.oauth2.resource.servlet.OAuth2ResourceServerAutoConfiguration"
+})
 @ActiveProfiles("test")
 class IntegracaoMtLoginOAuth2ApplicationTests {
 
